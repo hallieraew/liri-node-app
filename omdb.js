5a49b0d2c84ef0b8a217d675b871a8629a02139c
hallieraew <hallieweintraub@gmail.com>
2020-02-17 17:02:37 -0600

    connecting in to api for each

--- omdb.js
+++ omdb.js
@@ -1,11 +1,24 @@
-// Basic Node application for requesting data from the OMDB website via axios
-// Here we incorporate the "axios" npm package
+
 var axios = require("axios");
 var moveTitle = process.argv[2];
 // We then run the request with axios module on a URL with a JSON
 axios.get("http://www.omdbapi.com/?t=" + moveTitle +"&y=&plot=short&apikey=trilogy").then(
   function(response) {
     // Then we print out the imdbRating
-    console.log("The movie's rating is: " + response.data.imdbRating);
+    console.log("Movie Title: " + response.data.moveTitle);
+    console.log("\n-------------\n");
+    console.log("Release Year: " + response.data.Year);
+    console.log("\n-------------\n");
+    console.log("The movie's IMDB rating is: " + response.data.imdbRating);
+    console.log("\n-------------\n");
+    console.log("The movie's Rotten Tomatoes rating is: " + response.data.Ratings[1]);
+    console.log("\n-------------\n");
+    console.log("Produced in: " + response.data.Country);
+    console.log("\n-------------\n");
+    console.log("Language: " + response.data.Language);
+    console.log("\n-------------\n");
+    console.log("Plot: " + response.data.Plot);
+    console.log("\n-------------\n");
+    console.log("Actors: " + response.data.Actors)
   }
 );
--- spotify.js
+++ spotify.js
@@ -1,5 +1,25 @@
-var axios = require("axios");
-
+require("dotenv").config();
+var keys = require("./keys.js");
+var Spotify = require('node-spotify-api');
+var fs = require("fs");
 var spotify = new Spotify(keys.spotify);
+var search= process.argv[2];
 
-axios.get(spotify)
+fs.readFile("random.txt", "utf8", function(err, data) {
+    if (err) {
+        return console.log(err);
+    }
+    else {
+        
+        spotify
+        .search({ type: 'track', query: search })
+        .then(function(response) {
+            console.log(response);
+        })
+        .catch(function(err) {
+            console.log(err);
+        });
+    }
+});
+
+
