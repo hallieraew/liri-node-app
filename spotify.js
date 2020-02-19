a4e6578feba0dc1d5498a563b38c2fe480531014
hallieraew <hallieweintraub@gmail.com>
2020-02-19 00:43:44 -0600

    night commit

--- spotify.js
+++ spotify.js
@@ -2,24 +2,24 @@ require("dotenv").config();
 var keys = require("./keys.js");
 var Spotify = require('node-spotify-api');
 var fs = require("fs");
-var spotify = new Spotify(keys.spotify);
+var spotifyKey = new Spotify(keys.spotify);
 var search= process.argv[2];
 
-fs.readFile("random.txt", "utf8", function(err, data) {
-    if (err) {
-        return console.log(err);
-    }
-    else {
+// fs.readFile("random.txt", "utf8", function(err, data) {
+//     if (err) {
+//         return console.log(err);
+//     }
+//     else {
         
-        spotify
-        .search({ type: 'track', query: search })
-        .then(function(response) {
-            console.log(response);
-        })
-        .catch(function(err) {
-            console.log(err);
-        });
-    }
-});
-
-
+//     });
+// }
+    
+    
+    spotifyKey
+    .search({ type: 'track', query: search })
+    .then(function(response) {
+        console.log(response.track);
+    })
+    .catch(function(err) {
+        console.log(err);
+    });
