037ac6c9b9425f5380a93f44bd089b8dcc12c443
hallieraew <hallieweintraub@gmail.com>
2020-02-19 13:11:06 -0600

    working do what it says and concert

--- liri.js
+++ liri.js
@@ -8,9 +8,7 @@ var spotify = new Spotify(keys.spotify);
 
 var commandInput = process.argv[2];
 var searchInput = process.argv[3];
-var artist = searchInput;
-var songName = searchInput;
-var movieTitle = searchInput;
+
 
 switch (commandInput) {
     case "concert-this":
@@ -32,35 +30,37 @@ switch (commandInput) {
 
 // need to sort out json values
 function getConcert() {
+    var artist = searchInput;
     axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp").then(
         function (response) {
-            console.log("Venue: " + response.data[0].venue.name);
-            console.log("\n-------------\n");
-            console.log("Venue Location: " + response.data[0].venue.city);
-            console.log("\n-------------\n");
+            console.log("\nVenue: " + response.data[1].venue.name);
+            // console.log("\n-------------\n");
+            console.log("Venue Location: " + response.data[1].venue.city);
+            // console.log("\n-------------\n");
             var date = moment(response.data[0].datetime).format('MM/DD/YYYY');
             console.log("Date of Event: " + date);
-            console.log("\n-------------\n");
+            // console.log("\n-------------\n");
         }
-            .catch(function (err) {
-                console.log(err);
-            })
     )
+        .catch(function (err) {
+            console.log(err);
+        })
 };
 
 function getSong() {
+    var songName = searchInput;
 
-    if (songName === "") {
-        songName = "I Saw the Sign"
+    if (songName == "") {
+        songName = "The Sign"
     }
 
     spotify
         .search({ type: 'track', query: songName, limit: 1 })
         .then(function (response) {
-            console.log("\nTrack: " + songName);
-            console.log("\nArtist: " + response.tracks.items[0].album.artists[0].name);
-            console.log("\nSong Preview: " + response.tracks.items[0].preview_url);
-            console.log("\nAlbum: " + response.tracks.items[0].album.name + "\n")
+                console.log("\nTrack: " + songName);
+                console.log("\nArtist: " + response.tracks.items[0].album.artists[0].name);
+                console.log("\nSong Preview: " + response.tracks.items[0].preview_url);
+                console.log("\nAlbum: " + response.tracks.items[0].album.name + "\n")
         })
         .catch(function (err) {
             console.log(err);
@@ -68,23 +68,29 @@ function getSong() {
 };
 
 function getMovie() {
+    var movieTitle = searchInput;
+
+    if (movieTitle === "") {
+        movieTitle = "Mr. Nobody"
+    }
+
     axios.get("http://www.omdbapi.com/?t=" + movieTitle + "&y=&plot=short&apikey=trilogy").then(
         function (response) {
-            console.log("\n*Movie Title: " + response.data.Title);
-            console.log("\n-------------\n");
-            console.log("Release Year: " + response.data.Year);
-            console.log("\n-------------\n");
-            console.log("The movie's IMDB rating is: " + response.data.imdbRating);
-            console.log("\n-------------\n");
-            console.log("The movie's Rotten Tomatoes rating is: " + response.data.Ratings[1].Value);
-            console.log("\n-------------\n");
-            console.log("Produced in: " + response.data.Country);
-            console.log("\n-------------\n");
-            console.log("Language: " + response.data.Language);
-            console.log("\n-------------\n");
-            console.log("Plot: " + response.data.Plot);
-            console.log("\n-------------\n");
-            console.log("Actors: " + response.data.Actors)
+            console.log("Movie Title: " + response.data.Title);
+            // console.log("\n-------------\n");
+            console.log("\nRelease Year: " + response.data.Year);
+            // console.log("\n-------------\n");
+            console.log("\nThe movie's IMDB rating is: " + response.data.imdbRating);
+            // console.log("\n-------------\n");
+            console.log("\nThe movie's Rotten Tomatoes rating is: " + response.data.Ratings[1].Value);
+            // console.log("\n-------------\n");
+            console.log("\nProduced in: " + response.data.Country);
+            // console.log("\n-------------\n");
+            console.log("\nLanguage: " + response.data.Language);
+            // console.log("\n-------------\n");
+            console.log("\nPlot: " + response.data.Plot);
+            console.log("\nActors: " + response.data.Actors)
+            console.log("\n----------------------------\n");
         }
     ).catch(function (err) {
         console.log(err);
@@ -92,5 +98,36 @@ function getMovie() {
 };
 
 function doRandom() {
+    fs.readFile("random.txt", "utf8", function (err, data) {
+        if (err) {
+            return console.log(err);
+        }
+        console.log(data);
+        var dataArray = data.split(", ");
+        if (dataArray.length === 2) {
 
+            commandInput = dataArray[0];
+            searchInput = dataArray[1];
+
+            switch (commandInput) {
+                case "concert-this":
+                    getConcert(searchInput)
+                    break;
+
+                case "spotify-this-song":
+                    getSong(searchInput)
+                    break;
+
+                case "movie-this":
+                    getMovie(searchInput)
+                    break;
+
+                case "do-what-it-says":
+                    doRandom(searchInput)
+                    break;
+            };
+        } else {
+            console.log("That didn't work.")
+        }
+    })
 }
